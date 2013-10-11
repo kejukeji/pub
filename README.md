af02f1eaf391c95ddd409b4ae1235ad208a5ca85
eXthen <exthen@gmail.com>
2013-10-11 13:22:39 +0800

    重构

--- README.md
+++ README.md
@@ -7,6 +7,5 @@ urls.py
 pub_app.py
 
 这里服务器的配置文件需要做一下的改变：
-1. setting/server.cfg
-2. secure_key 需要改变
-3. develop_vars.py需要改变
+1. ex_var.py文件
+2. setting文件
---
+++ setting/lyw.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+# flask模块需要的配置参数
+# ===============================================================
+DEBUG = True  # 是否启动调试功能
+SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT^&556gh/ghj~hj/kh'  # session相关的密匙
+
+# models模块需要的配置参数
+# ===============================================================
+SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@127.0.0.1:3306/bar?charset=utf8'  # 连接的数据库
+SQLALCHEMY_ECHO = True  # 是否显示SQL语句
+
+# restfuls模块需要的配置参数
+# ===============================================================
+# 用户头像
+HEAD_PICTURE_UPLOAD_FOLDER = '/upload/head_picture'  # 运行目录的相对目录，URL获取图片的路径
+HEAD_PICTURE_BASE_PATH = '/Users/X/Dropbox/Code/pub'  # pub运行文件的目录，图片的绝对路径使用
+HEAD_PICTURE_ALLOWED_EXTENSION = ('png', 'jpg', 'jpeg', 'gif')  # 允许的拓展名
+
+# 其他配置参数
+# ===============================================================
+# 显示调试的框框
+DEBUG_TOOLBAR = True
