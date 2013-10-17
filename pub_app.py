6415d889e413160f53b6a549d65302d1a2fc66c8
eXthen <exthen@gmail.com>
2013-10-17 21:44:50 +0800

    路径统一管理

--- pub_app.py
+++ pub_app.py
@@ -12,24 +12,13 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 from flask import Flask
-from flask.ext import restful
 
 from models.database import db
-from urls import admin
-from login import login_manager, login, logout
-from restfuls import (UserInfo, UserLogin, UserRegister, PubGetType, PubListDetail, PubDetail, UserCollect,
-                      PubCollect, PubPictureDetail, PubSearch, GetPubType, GetProvince, GetCity, GetCounty,
-                      UserMessage)
 from ex_var import CONFIG_FILE
 
 # 创建应用
 app = Flask(__name__)
 app.config.from_pyfile(CONFIG_FILE)
-
-# 后台管理路径导入
-admin.init_app(app)
-# 用户登陆管理
-login_manager.init_app(app)
 
 # 自动关闭数据库连接
 @app.teardown_appcontext
@@ -38,32 +27,7 @@ def close_db(exception=None):
         print('++++' + str(exception) + '++++')
     db.remove()
 
-# todo-lyw 更好的路径管理插件，直接放在这里是不知道如何放在单独文件里面
-# 用户登陆相关路径
-app.add_url_rule('/login', 'login', login)
-app.add_url_rule('/logout', 'logout', logout)
-
-# todo-lyw 更好的接口管理，如何放置在单独文件
-# 接口文档路径管理
-api = restful.Api(app)
-api.add_resource(UserRegister, '/restful/user/register')
-api.add_resource(UserLogin, '/restful/user/login')
-api.add_resource(UserInfo, '/restful/user/user_info/<int:user_id>')
-api.add_resource(PubGetType, '/restful/pub/home')
-api.add_resource(PubListDetail, '/restful/pub/list/detail')
-api.add_resource(PubDetail, '/restful/pub/detail')
-api.add_resource(UserCollect, '/restful/user/collect')
-api.add_resource(PubCollect, '/restful/pub/collect')
-api.add_resource(PubPictureDetail, '/restful/pub/picture')
-api.add_resource(PubSearch, '/restful/pub/search')
-api.add_resource(UserMessage, '/restful/user/message')
-
-# 后台获取相关ajax文件的路径
-# todo-lyw 直辖市的市级只留下一个选项！
-api.add_resource(GetPubType, '/restful/admin/pub_type')
-api.add_resource(GetProvince, '/restful/admin/province')
-api.add_resource(GetCity, '/restful/admin/city/<int:province_id>')
-api.add_resource(GetCounty, '/restful/admin/county/<int:city_id>')
+from urls import *
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- urls.py
+++ urls.py
@@ -7,17 +7,52 @@
 import os
 
 from flask.ext.admin import Admin
+from flask.ext import restful
 
+from pub_app import app
 from models import db
 from views import UserView, PubTypeView, PubView, PubFile
+from login import login_manager, login, logout
+from restfuls import (UserInfo, UserLogin, UserRegister, PubGetType, PubListDetail, PubDetail, UserCollect,
+                      PubCollect, PubPictureDetail, PubSearch, GetPubType, GetProvince, GetCity, GetCounty,
+                      UserMessage)
 
-path = os.path.join(os.path.dirname(__file__), 'static')
+# 用户登陆管理
+login_manager.init_app(app)
+app.add_url_rule('/login', 'login', login)
+app.add_url_rule('/logout', 'logout', logout)
 
 # 后台管理系统路径管理
 admin = Admin(name=u'冒冒')
+admin.init_app(app)
 admin.add_view(UserView(db, name=u'用户'))
 
 admin.add_view(PubTypeView(db, name=u'酒吧类型', category=u'酒吧'))
 admin.add_view(PubView(db, name=u'酒吧详情', category=u'酒吧'))
 
+### 文件管理
+path = os.path.join(os.path.dirname(__file__), 'static')
 admin.add_view(PubFile(path, '/static/', name='文件'))
+
+# API接口
+api = restful.Api(app)
+
+### 后台获取相关ajax文件的路径
+api.add_resource(GetPubType, '/restful/admin/pub_type')
+api.add_resource(GetProvince, '/restful/admin/province')
+api.add_resource(GetCity, '/restful/admin/city/<int:province_id>')
+api.add_resource(GetCounty, '/restful/admin/county/<int:city_id>')
+
+### 接口文档路径管理
+api.add_resource(UserRegister, '/restful/user/register')
+api.add_resource(UserLogin, '/restful/user/login')
+api.add_resource(UserInfo, '/restful/user/user_info/<int:user_id>')
+api.add_resource(PubGetType, '/restful/pub/home')
+api.add_resource(PubListDetail, '/restful/pub/list/detail')
+api.add_resource(PubDetail, '/restful/pub/detail')
+api.add_resource(UserCollect, '/restful/user/collect')
+api.add_resource(PubCollect, '/restful/pub/collect')
+api.add_resource(PubPictureDetail, '/restful/pub/picture')
+api.add_resource(PubSearch, '/restful/pub/search')
+api.add_resource(UserMessage, '/restful/user/message')
+
