0fe97feb1c558956bf7fa04953cbf825b3c86a72
yuzhe0918 <xiangyuzhe0918@126.com>
2013-10-21 17:53:55 +0800

    后台私信，收藏，以及检查open_id接口。

--- urls.py
+++ urls.py
@@ -11,11 +11,11 @@ from flask.ext import restful
 
 from pub_app import app
 from models import db
-from views import UserView, PubTypeView, PubView, PubFile
+from views import UserView, PubTypeView, PubView, PubFile, UserMessageView, UserCollectView
 from login import login_manager, login, logout
 from restfuls import (UserInfo, UserLogin, UserRegister, PubGetType, PubListDetail, PubDetail, UserCollect,
                       PubCollect, PubPictureDetail, PubSearch, GetPubType, GetProvince, GetCity, GetCounty,
-                      UserMessage, PubSearchView, GetPubTypeList)
+                      UserMessage, PubSearchView, GetPubTypeList, UserOpenIdCheck)
 
 # 用户登陆管理
 login_manager.init_app(app)
@@ -26,6 +26,8 @@ app.add_url_rule('/logout', 'logout', logout)
 admin = Admin(name=u'冒冒')
 admin.init_app(app)
 admin.add_view(UserView(db, name=u'用户'))
+admin.add_view(UserMessageView(db, name=u'用户私信', category=u'私信/收藏'))
+admin.add_view(UserCollectView(db, name=u'用户收藏', category=u'私信/收藏'))
 
 admin.add_view(PubTypeView(db, name=u'酒吧类型', category=u'酒吧'))
 admin.add_view(PubView(db, name=u'酒吧详情', category=u'酒吧'))
@@ -48,6 +50,7 @@ api.add_resource(GetCounty, '/restful/admin/county/<int:city_id>')
 
 ### 接口文档路径管理
 api.add_resource(UserRegister, '/restful/user/register')
+api.add_resource(UserOpenIdCheck, '/restful/user/check')
 api.add_resource(UserLogin, '/restful/user/login')
 api.add_resource(UserInfo, '/restful/user/user_info/<int:user_id>')
 api.add_resource(PubGetType, '/restful/pub/home')
