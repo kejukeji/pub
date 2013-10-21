8e2496df65f9fbeb8cd99cd9759f65c4071ab768
eXthen <exthen@gmail.com>
2013-10-21 15:31:43 +0800

    测试服务器配置

--- setting/test_server.py
+++ setting/test_server.py
@@ -4,11 +4,12 @@
 # ===============================================================
 DEBUG = True  # 是否启动调试功能
 SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT^&556gh/ghj~hj/kh'  # session相关的密匙
-#SERVER_NAME = 'pub.app:5001'
+SERVER_NAME = 'pub.app:5001'
 
 # models模块需要的配置参数
 # ===============================================================
 SQLALCHEMY_DATABASE_URI = 'mysql://root:root@127.0.0.1:3306/maobake_pub?charset=utf8'  # 连接的数据库
+#SQLALCHEMY_DATABASE_URI = 'mysql://root:root@42.121.108.142:3306/maobake_pub?charset=utf8'  # 连接的数据库
 SQLALCHEMY_ECHO = True  # 是否显示SQL语句
 
 # restfuls模块需要的配置参数
