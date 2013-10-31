cbdc91ab2439d14264db4040435ec3f691ff5384
eXthen <exthen@gmail.com>
2013-10-31 12:55:59 +0800

    put_type添加字段，这里改写初始化和更新函数

--- models/pub.py
+++ models/pub.py
@@ -117,10 +117,16 @@ class PubType(Base):
     def __init__(self, **kwargs):
         self.name = kwargs.pop('name')
         self.code = kwargs.pop('code')
+        self.base_path = kwargs.pop('base_path', None)
+        self.rel_path = kwargs.pop('rel_path', None)
+        self.pic_name = kwargs.pop('pic_name', None)
 
     def update(self, **kwargs):
         self.name = kwargs.pop('name')
         self.code = kwargs.pop('code')
+        self.base_path = kwargs.pop('base_path', None)
+        self.rel_path = kwargs.pop('rel_path', None)
+        self.pic_name = kwargs.pop('pic_name', None)
 
     def __repr__(self):
         return '<PubType(name: %s, code: %s)>' % (self.name, self.code)
