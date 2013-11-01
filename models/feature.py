e584080fc3a124ae29f78d9a2b8fdfc5a042a2de
yuzhe0918 <xiangyuzhe0918@126.com>
2013-11-01 13:35:16 +0800

    意见反馈。

--- models/feature.py
+++ models/feature.py
@@ -19,6 +19,7 @@ COLLECT_TABLE = 'collect'
 COMMENT_TABLE = 'comment'
 VIEW_TABLE = 'view'
 MESSAGE_TABLE = 'message'
+FEED_BACK = 'feed_back'
 
 
 class Collect(Base):
@@ -62,6 +63,7 @@ class Comment(Base):
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey(User.id, ondelete='set null', onupdate='cascade'), nullable=True)
     pub_id = Column(Integer, ForeignKey(Pub.id, ondelete='cascade', onupdate='cascade'), nullable=False)
+    pub = relationship(Pub)
     time = Column(DATETIME, nullable=False, server_default=None)
     content = Column(String(256), nullable=False)
     star = Column(Integer, nullable=False, server_default='5')
@@ -133,3 +135,17 @@ class Message(Base):
     def __repr__(self):
         return '<Message(sender_id: %s, receiver_id: %s, content: %s)>' % \
                (self.sender_id, self.receiver_id, self.content)
+
+
+class FeedBack(Base):
+    """
+        意见反馈
+            id: 主键
+            content：内容
+    """
+    __tablename__ = FEED_BACK
+    id = Column(Integer, primary_key=True)
+    content = Column(String(140), nullable=True)
+
+    def __init__(self, **kwargs):
+        self.content = kwargs.pop('content')
