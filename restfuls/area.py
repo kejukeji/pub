57b08c3085b0aba5af027c181dc79be11752e350
yuzhe0918 <xiangyuzhe0918@126.com>
2013-11-12 12:46:06 +0800

    省市修改。

--- restfuls/area.py
+++ restfuls/area.py
@@ -2,8 +2,8 @@
 #!/usr/bin/python
 
 from flask.ext import restful
-from models import Province, City
-from utils.others import success_dic, fail_dic, pickler
+from models import Province, City, County
+from utils.others import success_dic, fail_dic, flatten
 
 
 class Area(restful.Resource):
@@ -12,16 +12,38 @@ class Area(restful.Resource):
     def get():
         resp_suc = success_dic().dic
         resp_fail = fail_dic().dic
+        resp_suc['list'] = []
 
-        provinces = Province.query.filter().all()
-        citys = City.query.filter().all()
-
-        resp_suc['province_list'] = []
-        resp_suc['city_list'] = []
-        for province in provinces:
-            province_pic = pickler.flatten(province)
-            resp_suc['province_list'].append(province_pic)
-        for city in citys:
-            city_pic = pickler.flatten(city)
-            resp_suc['city_list'].append(city_pic)
+        resp_suc = get_province_city(resp_suc)
         return resp_suc
+
+
+def get_province_city(resp_suc):
+    '''获取省市'''
+    provinces = Province.query.filter().all()
+    citys = City.query.filter().all()
+    countys = County.query.filter().all()
+    for province in provinces:
+        province_pic = flatten(province)
+        province_pic['city_list'] = []
+        for city in citys:
+            if city.province_id == 1 and province.id == 1:
+                get_county(countys, city.id, province_pic)
+            elif city.province_id == 2 and province.id == 2:
+                get_county(countys, city.id, province_pic)
+            elif city.province_id == 9 and province.id == 9:
+                get_county(countys, city.id, province_pic)
+            elif city.province_id == 22 and province.id == 22:
+                get_county(countys, city.id, province_pic)
+            elif city.province_id == province.id:
+                city_pic = flatten(city)
+                province_pic['city_list'].append(city_pic)
+        resp_suc['list'].append(province_pic)
+    return resp_suc
+
+def get_county(countys,city_id, province_pic):
+    '''得到直辖市的区'''
+    for county in countys:
+        if county.city_id == city_id:
+            county_pic = flatten(county)
+            province_pic['city_list'].append(county_pic)
